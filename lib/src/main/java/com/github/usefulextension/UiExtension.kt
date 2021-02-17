b31e14574f8e496f52793cdbf14816b65524b886
meysam <mohamadian.meysam@gmail.com>
2021-02-17 18:52:40 +0330

    add ui extension

---
+++ lib/src/main/java/com/github/usefulextension/UiExtension.kt
@@ -0,0 +1,82 @@
+package com.github.usefulextension
+
+import android.app.Activity
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.annotation.StringRes
+import androidx.fragment.app.Fragment
+
+/**
+* hide keyboard in fragment
+*/
+fun Fragment.hideKeyboard() {
+    view?.let { activity?.hideKeyboard(it) }
+}
+
+/**
+* hide keyboard in Activity
+*/
+fun Activity.hideKeyboard() {
+    hideKeyboard(currentFocus ?: View(this))
+}
+
+/**
+* hide keyboard
+*/
+fun Context.hideKeyboard(view: View) {
+    val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+    inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+}
+
+/**
+* quick toast in fragment
+* Toast.LENGTH_LONG
+*/
+fun Fragment.longToast(@StringRes res: Int) {
+    Toast.makeText(requireContext(), getString(res), Toast.LENGTH_LONG).show()
+}
+
+
+/**
+* quick toast in fragment
+* Toast.LENGTH_SHORT
+*/
+fun Fragment.shortToast(@StringRes res: Int) {
+    Toast.makeText(requireContext(), getString(res), Toast.LENGTH_SHORT).show()
+}
+
+/**
+* quick toast in activity
+* Toast.LENGTH_LONG
+*/
+fun Activity.longToast(@StringRes res: Int) {
+    Toast.makeText(this, getString(res), Toast.LENGTH_LONG).show()
+}
+
+
+/**
+* quick toast in activity
+* Toast.LENGTH_SHORT
+*/
+fun Activity.shortToast(@StringRes res: Int) {
+    Toast.makeText(this, getString(res), Toast.LENGTH_SHORT).show()
+}
+
+/**
+* quick toast
+* Toast.LENGTH_LONG
+*/
+fun Context.longToast(@StringRes res: Int) {
+    Toast.makeText(this, getString(res), Toast.LENGTH_LONG).show()
+}
+
+/**
+* quick toast
+* Toast.LENGTH_SHORT
+*/
+fun Context.shortToast(@StringRes res: Int) {
+    Toast.makeText(this, getString(res), Toast.LENGTH_SHORT).show()
+}
+
