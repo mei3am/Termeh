5a9d0dedc71dd668a2a637edefa760b713e4b3de
meysam <mohamadian.meysam@gmail.com>
2021-02-22 19:25:19 +0330

    add more extension

--- lib/src/main/java/com/github/usefulextension/PrimitiveExtention.kt
+++ lib/src/main/java/com/github/usefulextension/PrimitiveExtention.kt
@@ -2,8 +2,10 @@ package com.github.usefulextension
 
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
+import java.security.MessageDigest
 import java.security.SecureRandom
 import java.text.DecimalFormat
+import java.util.regex.Pattern
 
 /**
  * check if number is odd
@@ -86,9 +88,58 @@ fun String?.toSafeInt(): Int? = run {
     }
 }
 
-val String.isDigit: Boolean
-    get() = matches(Regex(".*[0-9].*"))
-
+/**
+ * check is string contain only alphabet and digits
+ * */
 val String.isAlphanumeric: Boolean
     get() = matches(Regex("[a-zA-Z0-9]*"))
 
+/**
+ * check is string contain latin letter
+ * */
+val String.containsLatinLetter: Boolean
+    get() = matches(Regex(".*[A-Za-z].*"))
+
+/**
+ * check is string contain only persian letter
+ * */
+val String.containsOnlyPersianLetter: Boolean
+    get() = matches(Regex("^[\\u0622\\u0627\\u0628\\u067E\\u062A-\\u062C\\u0686\\u062D-\\u0632\\u0698\\u0633-\\u063A\\u0641\\u0642\\u06A9\\u06AF\\u0644-\\u0648\\u06CC]+\$"))
+
+/**
+ * check is string contain only digits
+ * */
+val String.containsDigit: Boolean
+    get() = matches(Regex(".*[0-9].*"))
+
+
+/**
+ * Checking validity of an email address
+* */
+fun String.isValidEmail(): Boolean {
+    val expression = "^[\\w.-]+@([\\w\\-]+\\.)+[A-Z]{2,8}$"
+    val pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE)
+    val matcher = pattern.matcher(this)
+    return matcher.matches()
+}
+
+/**
+ * Checking validity of an iranian mobile number
+* */
+fun String.isValidIranianMobile(): Boolean {
+    val expression = "^(\\+98|0)?9\\d{9}\$"
+    val pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE)
+    val matcher = pattern.matcher(this)
+    return matcher.matches()
+}
+
+/**
+ * generate sha hash
+ */
+val String.sha1: String
+    get() {
+        val bytes = MessageDigest.getInstance("SHA-1").digest(this.toByteArray())
+        return bytes.joinToString("") {
+            "%02x".format(it)
+        }
+    }
--- lib/src/main/java/com/github/usefulextension/UiExtension.kt
+++ lib/src/main/java/com/github/usefulextension/UiExtension.kt
@@ -2,28 +2,30 @@ package com.github.usefulextension
 
 import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.view.View
+import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
 
 /**
-* hide keyboard in fragment
+* hide keyboard in [Fragment ]
 */
 fun Fragment.hideKeyboard() {
     view?.let { activity?.hideKeyboard(it) }
 }
 
 /**
-* hide keyboard in Activity
+* hide keyboard in [Activity]
 */
 fun Activity.hideKeyboard() {
     hideKeyboard(currentFocus ?: View(this))
 }
 
 /**
-* hide keyboard
+* hide keyboard [Context]
 */
 fun Context.hideKeyboard(view: View) {
     val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
@@ -31,7 +33,7 @@ fun Context.hideKeyboard(view: View) {
 }
 
 /**
-* quick toast in fragment
+* quick toast in [Fragment]
 * Toast.LENGTH_LONG
 */
 fun Fragment.longToast(@StringRes res: Int) {
@@ -40,7 +42,7 @@ fun Fragment.longToast(@StringRes res: Int) {
 
 
 /**
-* quick toast in fragment
+* quick toast in [Fragment]
 * Toast.LENGTH_SHORT
 */
 fun Fragment.shortToast(@StringRes res: Int) {
@@ -48,7 +50,7 @@ fun Fragment.shortToast(@StringRes res: Int) {
 }
 
 /**
-* quick toast in activity
+* quick toast in [Activity]
 * Toast.LENGTH_LONG
 */
 fun Activity.longToast(@StringRes res: Int) {
@@ -57,7 +59,7 @@ fun Activity.longToast(@StringRes res: Int) {
 
 
 /**
-* quick toast in activity
+* quick toast in [Activity]
 * Toast.LENGTH_SHORT
 */
 fun Activity.shortToast(@StringRes res: Int) {
@@ -80,3 +82,53 @@ fun Context.shortToast(@StringRes res: Int) {
     Toast.makeText(this, getString(res), Toast.LENGTH_SHORT).show()
 }
 
+/**
+ * start new activity in [Activity]
+ * */
+inline fun <reified T : Activity> Activity.startActivity() {
+    val intent = Intent(this, T::class.java)
+    startActivity(intent)
+}
+
+/**
+ * start new activity in [Fragment]
+ * */
+inline fun <reified T : Activity> Fragment.startActivity() {
+    val intent = Intent(this.requireActivity(), T::class.java)
+    startActivity(intent)
+}
+
+/**
+ * visible view
+ */
+fun View.visible() {
+    visibility = View.VISIBLE
+}
+
+/**
+ * invisible view
+ */
+fun View.invisible() {
+    visibility = View.INVISIBLE
+}
+
+/**
+ * gone view
+ */
+fun View.gone() {
+    visibility = View.GONE
+}
+
+/**
+ * disable view
+ */
+fun View.disable() {
+    isEnabled  = false
+}
+
+/**
+ * enable view
+ */
+fun View.enable() {
+    isEnabled = true
+}
